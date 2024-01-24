f03bebc49fc8c233e0c710813eaa0272bd8e8d4f
EMANDI DEVI PRANEETH <praneethe4@gmail.com>
2024-01-24 19:47:29 +0530

    banner

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,9 +1,11 @@
 import React from "react";
-import banner from "./images/NITC.jpeg";
+import banner from "./images/levyug.jpeg";
 
 function Banner() {
   return (
+    <div className="banner-con">
     <img  className="banner" src={banner} alt="NITC Banner" />
+    </div>
   );
 }
 
